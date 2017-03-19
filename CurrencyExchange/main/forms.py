69be99a024987c83f2adf500bb85d779b115e380
Kagan <bkaganyildiz@github.com>
2017-03-19 16:04:15 +0200

    Currency Form has been created init method has been overriden

---
+++ CurrencyExchange/main/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+import requests
+def getCurrencyChoices():
+    response = requests.get('http://api.fixer.io/latest')
+    currencies = response.json()
+    returner =[]
+    returner.append((currencies['base'],currencies['base']))
+    for cur in currencies['rates'] :
+        returner.append((cur,cur))
+    return returner
+class CurrencyConverterForm(forms.Form) :
+    currency1 = forms.ChoiceField(required=True,label="From Currency")
+    currency2 = forms.ChoiceField(required=True, label="To Currency")
+    amount    = forms.TextField(widget=forms.TextInput(attrs={'type':'number'}))
+    def __init__(self, *args, **kwargs):
+        super(CurrencyConverterForm, self).__init__(*args, **kwargs)
+        self.fields['currency1'].choices = getCurrencyChoices()
+        self.fields['currency2'].choices = getCurrencyChoices()
