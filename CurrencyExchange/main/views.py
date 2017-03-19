884d26fad0fc0135c904ac2e9abc661c7b50d0fd
Kagan <bkaganyildiz@github.com>
2017-03-19 16:02:43 +0200

    Form has been added

--- CurrencyExchange/main/views.py
+++ CurrencyExchange/main/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+from .forms import CurrencyConverterForm
 
+@csrf_exempt
 def index(request) :
+    form = CurrencyConverterForm(request.POST or None)
+    if form.is_valid():
+
     return  None
 # Create your views here.
